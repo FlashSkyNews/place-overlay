c3346015b0ab20d1e68c3bce79c110a7cf753032
terwarf <terwarf@googlemail.com>
2023-07-22 22:39:31 +0200

    Add toggle button for the overlay

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -11,6 +11,7 @@
 // ==/UserScript==
 
 var overlayImage = null;
+var overlayButton = null;
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
@@ -18,11 +19,27 @@ if (window.top !== window.self) {
         updateImage();
         overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;pointerEvents: 'none';`;
         canvasContainer.appendChild(overlayImage);
+        overlayButton = document.createElement("button");
+        overlayButton.style = "position: absolute; top: 28px;left: 80px;border-radius: 0;border: 3px solid black;padding: 4px 10px;height: fit-content;";
+        overlayButton.textContent = "Overlay: an";
+        overlayButton.addEventListener('click', () => {toggleOverlay();});
+        window.document.body.append( overlayButton );
     }, false);
 }
 
 function updateImage() {
     overlayImage.src = "https://place.kayo.zip/outputs/overlay_target.png?" + Date.now()
 }
+
+function toggleOverlay() {
+    if( overlayImage.style.display == "none" ) {
+        overlayImage.style.display = "block";
+        overlayButton.textContent = "Overlay: an";
+    } else {
+        overlayImage.style.display = "none";
+        overlayButton.textContent = "Overlay: aus";
+    }
+}
+
 
 setInterval(function () {overlayImage.src = "https://place.kayo.zip/outputs/overlay_target.png?" + Date.now()}, 30000);
