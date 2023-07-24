2ea46b8e645c07b927540c8feddb4f99e440557a
Ju-Ko <97811075+Ju-Ko@users.noreply.github.com>
2023-07-24 18:39:03 +0200

    new advanced overlay

--- src/scripts/advanced_overlay.user.js
+++ src/scripts/advanced_overlay.user.js
@@ -1,18 +1,17 @@
 // ==UserScript==
 // @name         r/papaplatte Advanced Overlay
 // @namespace    http://tampermonkey.net/
-// @version      1.1.1
+// @version      1.2
 // @description  Script that adds a button to toggle an hardcoded image shown in the 2023's r/place canvas
-// @author       max-was-here
+// @author       r/place canada, r/placeDE, kayo, juko
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.redditstatic.com/desktop2x/img/favicon/favicon-32x32.png
 // @grant        none
-// @updateURL    https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/advanced_overlay.js
-// @downloadURL  https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/advanced_overlay.js
+// @updateURL    https://github.com/FlashSkyNews/place-overlay/raw/main/src/scripts/advanced_overlay.user.js
+// @downloadURL  https://github.com/FlashSkyNews/place-overlay/raw/main/src/scripts/advanced_overlay.user.js
+// @run-at   document-start
 // ==/UserScript==
 
-let width = "2500px";
-let height = "2000px";
 var button = null;
 
 if (window.top !== window.self) {
@@ -20,11 +19,11 @@ if (window.top !== window.self) {
     // ==============================================
     const STORAGE_KEY = 'place-germany-2023-ostate';
     const OVERLAYS = [
-      ["https://place.kayo.zip/outputs/overlay_target.png", "Pixel"],
-      ["https://place.kayo.zip/outputs/default_target.png", "Vollbild"]
+      ["https://place.kayo.zip/outputs/overlay_target.png", "Kleine Pixel"],
+      ["https://place.kayo.zip/outputs/default_target.png", "Vollbild Pixel"]
     ];
     const getConfig = (text) => {
-        return text + "?" + Date.now()
+      return text + "?" + Date.now()
     }
 
     let oState = {
@@ -33,27 +32,35 @@ if (window.top !== window.self) {
     };
 
     const oStateStorage = localStorage.getItem(STORAGE_KEY);
-    if(oStateStorage !== null) {
+    if (oStateStorage !== null) {
       try {
         oState = Object.assign({}, oState, JSON.parse(oStateStorage));
-      } catch(e){}
+      } catch { }
     }
+
+
 
     const img = document.createElement('img');
     img.style.pointerEvents = 'none';
     img.style.position = 'absolute';
     img.style.imageRendering = 'pixelated';
-    img.src = OVERLAYS[oState.overlayIdx][0];
     img.style.opacity = oState.opacity;
     img.style.top = '0px';
     img.style.left = '0px';
-    img.style.width = width;
-    img.style.height = height;
     img.style.zIndex = '100';
     img.onload = () => {
-      console.log('loaded');
+      console.log('[Kewin] img loaded');
       img.style.opacity = oState.opacity / 100;
     };
+
+    const updateImage = () => {
+      img.src = getConfig(OVERLAYS[oState.overlayIdx][0])
+      console.log("[Kewin] updated overlay image")
+    };
+
+    updateImage();
+
+    setInterval(updateImage, 30000);
 
     const mainContainer = document
       .querySelector('garlic-bread-embed')
@@ -64,12 +71,33 @@ if (window.top !== window.self) {
     positionContainer.appendChild(img);
 
     // ==============================================
+    // Canvas size observer
+
+    const canvas = positionContainer.querySelector("canvas");
+    const canvasObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.type === "attributes") {
+          img.style.width = mutation.target.getAttribute("width") + "px";
+          img.style.height = mutation.target.getAttribute("height") + "px";
+        }
+      });
+    });
+
+    canvasObserver.observe(canvas, {
+        attributes: true
+    });
+
+    // ==============================================
     // Add buttons to toggle overlay
 
     const buttonsWrapper = document.createElement('div');
     buttonsWrapper.style.position = "absolute";
     buttonsWrapper.style.bottom = "25px";
     buttonsWrapper.style.right = "25px";
+    buttonsWrapper.style.display = "flex";
+    buttonsWrapper.style.flexDirection = "column";
+    buttonsWrapper.style.gap = "12px";
+
     mainContainer.appendChild(buttonsWrapper);
 
     const saveState = () => {
@@ -83,26 +111,42 @@ if (window.top !== window.self) {
     };
 
     const switchOverlay = () => {
-      oState.overlayIdx++;
-      if(oState.overlayIdx >= OVERLAYS.length){
-        oState.overlayIdx = 0;
-      }
-      img.src = getConfig(OVERLAYS[oState.overlayIdx][0]);
+      oState.overlayIdx = (oState.overlayIdx + 1) % OVERLAYS.length
+      updateImage();
       button.innerText = 'Switch Overlay\n(' + OVERLAYS[oState.overlayIdx][1] + ')';
       img.style.opacity = oState.opacity / 100;
       saveState();
     };
 
+    const exportScreenshot = () => {
+      const canvas = mainContainer
+        .querySelector('garlic-bread-canvas')
+        .shadowRoot.querySelector('canvas');
+      if (!canvas) {
+        return;
+      }
+      const imgUrl = canvas
+        .toDataURL('image/png');
+
+      const downloadEl = document
+        .createElement('a');
+      downloadEl.href = imgUrl;
+      downloadEl.download = `place-${Date.now()}.png`;
+      downloadEl.click();
+      downloadEl.remove();
+    }
+
     const addButton = (text, onClick) => {
       button = document.createElement('button');
       button.onclick = onClick;
       button.style.width = "100px";
-      button.style.height = "65px";
+      button.style.padding = "8px";
       button.style.backgroundColor = "#555";
       button.style.color = "white";
       button.style.border = "var(--pixel-border)";
       button.style.boxShadow = "var(--pixel-box-shadow)";
       button.style.fontFamily = "var(--garlic-bread-font-pixel)";
+      button.style.cursor = "pointer";
 
       button.innerText = text;
 
@@ -118,7 +162,6 @@ if (window.top !== window.self) {
       opacityWrapper.style.border = "var(--pixel-border)";
       opacityWrapper.style.boxShadow = "var(--pixel-box-shadow)";
       opacityWrapper.style.fontFamily = "var(--garlic-bread-font-pixel)";
-      opacityWrapper.style.marginTop = "15px";
       opacityWrapper.style.textAlign = "center";
       opacityWrapper.innerText = text;
 
@@ -141,7 +184,11 @@ if (window.top !== window.self) {
     };
 
     addButton(
-      'Switch Overlay\n(Pixel)',
+      'Screenshot',
+      exportScreenshot
+    )
+    addButton(
+      'Switch Overlay\n(kleine Pixel)',
       switchOverlay
     );
     addSlider(
